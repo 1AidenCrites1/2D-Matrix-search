e77e342fc308c7b0665a34ca740200dac2bcbb44
1AidenCrites1 <70043483+1AidenCrites1@users.noreply.github.com>
2021-12-13 10:06:33 -0500

    Add files via upload

---
+++ 2Dlist.py
@@ -0,0 +1,19 @@
+matrix = [[1, 4,], [22, 6, 98], [12, 3, 62], [1293, 322, 100]]
+target = int(input("Choose a target number: "))
+
+import bisect
+lst = []
+
+#converts nested list into a list so it can be searched
+for elem in matrix:
+    lst.extend(elem)
+
+matrix = lst
+
+#searches through matrix using bisect
+bisect.bisect(matrix, target)
+
+#checks to see if target is in matrix
+found = target in matrix
+print(found)
+    
